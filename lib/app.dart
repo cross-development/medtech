fb72d5f5cb0ab5b5645755f08e6a2b9d6c0a093e
Vitalii Derda <derdavitaliy@gmail.com>
2022-07-21 18:30:11 +0300

    add text button widget

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MedTech extends StatelessWidget {
+  const MedTech({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MedTech',
+      theme: ThemeData(),
+      home: null,
+    );
+  }
+}
